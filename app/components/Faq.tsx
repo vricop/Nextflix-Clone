c420cc489e405405dab8c817e4fd95b0a6f02d79
Víctor Rico <vricop@gmail.com>
2022-03-04 17:22:38 +0100

    Add <EmailBox /> & <Cta /> in <Faq />

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -1,17 +1,28 @@
 import { faq } from '@/fixtures/faq'
 import styled from 'styled-components'
 import { Accordion } from './Accordion'
+import { EmailBox } from './common/EmailBox'
 import { HomeContainer } from './common/HomeContainer'
+import { Cta } from './HeroHeader/HeroHeader.styled'
 
 const FaqContainer = styled(HomeContainer)`
   display: grid;
   grid-auto-flow: row;
-  row-gap: var(--size-8);
+  justify-items: center;
+  row-gap: var(--size-14);
 `
 
 const Title = styled.h2`
   text-align: center;
   font-size: var(--text-5xl);
+  line-height: var(--leading-tighter);
+`
+
+const CTAWrapper = styled.div`
+  text-align: center;
+  display: grid;
+  justify-content: center;
+  row-gap: var(--size-5);
 `
 
 export function Faq() {
@@ -23,6 +34,12 @@ export function Faq() {
           <Accordion.Item {...faqProps} />
         ))}
       </Accordion>
+      <CTAWrapper>
+        <Cta>
+          Ready to watch? Enter your email to create or restart your membership.
+        </Cta>
+        <EmailBox />
+      </CTAWrapper>
     </FaqContainer>
   )
 }
