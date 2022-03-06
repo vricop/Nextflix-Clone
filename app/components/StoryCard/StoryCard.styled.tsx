44aaef99704e24dfe526718d9feecd681fc4abb6
Víctor Rico <vricop@gmail.com>
2022-03-06 20:03:25 +0100

    Add responsive & use new design tokens from theme

--- app/components/StoryCard/StoryCard.styled.tsx
+++ app/components/StoryCard/StoryCard.styled.tsx
@@ -2,28 +2,51 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   align-items: center;
-  column-gap: var(--size-12);
+  column-gap: ${({ theme }) => theme.size['12']};
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
   margin-inline: auto;
   max-width: 68.75rem;
+
+  @media (min-width: 59.375rem) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `
 
 export const Title = styled.h2`
-  font-size: var(--text-5xl);
-  line-height: var(--leading-tighter);
+  font-size: ${props => props.theme.text['2xl']};
+  line-height: ${({ theme }) => theme.leading.tighter};
+
+  @media (min-width: 34.375rem) {
+    font-size: ${({ theme }) => theme.text['5xl']};
+  }
 `
 
 export const SubTitle = styled.h3`
-  font-size: var(--text-2xl);
-  font-weight: var(--font-normal);
+  font-size: ${({ theme }) => theme.text.lg};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+
+  @media (min-width: 34.375rem) {
+    font-size: ${({ theme }) => theme.text['2xl']};
+  }
 `
 
 export const Text = styled.hgroup<{ flipped: boolean }>`
   display: grid;
   grid-auto-flow: row;
-  row-gap: var(--size-5);
-  order: ${({ flipped }) => flipped && '2'};
+  row-gap: ${({ theme }) => theme.size[3]};
+
+  @media (min-width: 34.375rem) {
+    row-gap: ${({ theme }) => theme.size[5]};
+  }
+
+  @media (min-width: 59.375rem) {
+    row-gap: ${({ theme }) => theme.size[5]};
+    order: ${({ flipped }) => flipped && '2'};
+  }
+
+  @media (max-width: 59.375rem) {
+    text-align: center;
+  }
 `
 
 export const Media = styled.div`
