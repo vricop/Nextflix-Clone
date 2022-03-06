f935f8ac6f68613f598aafe27a0cc6c2c0aaca77
Víctor Rico <vricop@gmail.com>
2022-03-06 20:00:26 +0100

    Add responseive
    - Replace custom properties with design tokens in theme
    - Add resposive tweaks

--- app/components/common/HomeContainer.tsx
+++ app/components/common/HomeContainer.tsx
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
   background-color: black;
-  padding: var(--size-18) var(--size-11);
-  color: var(--color-white);
-  border-bottom: var(--size-2) solid var(--color-gray-600);
+  padding: ${({ theme }) => theme.size['12']} 5%;
+  color: ${({ theme }) => theme.color.white};
+  border-bottom: ${({ theme }) =>
+    `${theme.size['2']} solid ${theme.color['gray-600']}`};
+
+  @media (min-width: 34.375rem) {
+    padding: ${({ theme }) => `${theme.size['18']} ${theme.size['11']}`};
+  }
 `
