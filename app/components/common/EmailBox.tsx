8140fa19f879def3ab468d4b480c8f70c3a0d04a
Víctor Rico <vricop@gmail.com>
2022-03-06 13:34:07 +0100

    Add responsive to <EmailBox />

--- app/components/common/EmailBox.tsx
+++ app/components/common/EmailBox.tsx
@@ -2,12 +2,35 @@ import { NavArrowRight } from 'iconoir-react'
 import { ChangeEvent, ReactNode, useState } from 'react'
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Form = styled.form`
+  --height: 3rem;
+  --offset: 0.625rem;
+  grid-template:
+    'input' auto
+    'error' auto
+    'button' auto / auto;
   display: grid;
-  grid-template: 'input button' auto / min-content max-content;
-  justify-items: start;
-  row-gap: var(--size-1);
+  row-gap: var(--size-3);
+  justify-items: center;
   position: relative;
+
+  @media (max-width: 59.375rem) {
+    width: 100%;
+    max-width: 500px;
+  }
+
+  @media (min-width: 34.375rem) {
+    --height: 3.75rem;
+  }
+
+  @media (min-width: 59.375rem) {
+    --height: 4.375rem;
+    justify-items: start;
+    grid-template:
+      'input button' var(--height)
+      'error  error' auto / min-content max-content;
+    row-gap: var(--size-1);
+  }
 `
 
 export const EmailLabel = styled.label`
@@ -22,12 +45,23 @@ export const EmailLabel = styled.label`
 export const borderStyles = 'var(--size-0-5) solid var(--color-orange)'
 
 export const EmailField = styled.input`
+  border-radius: var(--size-0-5);
   grid-area: input;
-  line-height: var(--leading-17);
-  padding-inline: var(--size-3);
-  width: 500px;
+  padding: var(--offset) var(--size-3) 0;
+  width: 100%;
+  height: var(--height);
   border: none;
-  border-radius: var(--size-0-5) 0 0 var(--size-0-5);
+
+  @media (min-width: 59.375rem) {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    width: 500px;
+    height: initial;
+  }
+
+  @media (min-width: 34.375rem) {
+    line-height: calc(var(--height) - var(--offset));
+  }
 
   & + label {
     transition: all 200ms ease-in-out;
@@ -37,8 +71,12 @@ export const EmailField = styled.input`
   &:focus + label {
     top: var(--size-1);
     transform: translateY(0);
-    font-size: var(--text-xs);
+    font-size: var(--text-sm);
     font-weight: var(--font-bold);
+
+    @media (max-width: 34.375rem) {
+      font-size: var(--text-xs);
+    }
   }
 
   border-bottom: ${({ error }: { error?: boolean }) => error && borderStyles};
@@ -53,38 +91,54 @@ const Button = ({ children, ...rest }: { children: ReactNode }) => (
 export const GetStartedButton = styled(Button)`
   align-items: center;
   background-color: var(--color-brand-alt-light);
-  border-radius: 0 var(--size-0-5) var(--size-0-5) 0;
+  border-radius: var(--size-0-5);
   border: none;
   color: var(--color-wite);
   column-gap: var(--size-1);
   display: grid;
-  font-size: var(--text-3xl);
   grid-area: button;
   grid-auto-columns: min-content min-content;
   grid-auto-flow: column;
   justify-content: center;
-  line-height: var(--leading-17);
-  padding-inline: var(--size-8);
+  line-height: var(--leading-10);
+  padding-inline: var(--size-3);
   white-space: nowrap;
-  width: 100%;
+  width: auto;
   transition: 200ms ease-in-out;
+
+  @media (min-width: 59.375rem) {
+    border-top-left-radius: 0;
+    leading: var(--height);
+    border-bottom-left-radius: 0;
+    font-size: var(--text-2xl);
+    padding-inline: var(--size-8);
+  }
+
+  @media (min-width: 90.625rem) {
+    font-size: var(--text-3xl);
+
+    & > svg {
+      font-size: var(--text-2xl);
+    }
+  }
 
   &:hover {
     cursor: pointer;
     background-color: var(--color-brand);
   }
-
-  & > svg {
-    font-size: var(--text-2xl);
-  }
 `
 
 export const Error = styled.span`
-  position: absolute;
-  left: 0;
-  bottom: calc(var(--size-1) * -1);
   color: var(--color-orange);
-  transform: translateY(100%);
+  grid-area: error;
+  justify-self: start;
+  text-align: left;
+  line-height: var(--leading-snug);
+
+  @media (min-width: 59.375rem) {
+    position: absolute;
+    justify-self: initial;
+  }
 `
 
 export function EmailBox() {
@@ -97,8 +151,9 @@ export function EmailBox() {
   }
 
   return (
-    <Container>
+    <Form method="get">
       <EmailField
+        error={errorMessage !== ''}
         pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
         value={email}
         className={email && 'has-text'}
@@ -113,6 +168,6 @@ export function EmailBox() {
       <EmailLabel htmlFor="email">Email address</EmailLabel>
       <GetStartedButton>Get Started</GetStartedButton>
       {errorMessage && <Error>{errorMessage}</Error>}
-    </Container>
+    </Form>
   )
 }
